3fefdfacf6e403345390caf7892d85cb1a9d89c8
Tom Forbes <tom@tomforb.es>
2023-01-26 17:30:01 +0000

    {"name":"abceshi-congling-daoyi","version":"2023.1.27.1","file":"abceshi-congling-daoyi-2023.1.27.1.tar.gz","path":"abceshi-congling-daoyi/abceshi-congling-daoyi-2023.1.27.1.tar.gz"}

---
+++ abceshi-congling-daoyi/abceshi-congling-daoyi-2023.1.27.1.tar.gz/AbceshiConglingDaoyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""abceshi-congling-daoyi
+https://github.com/apachecn/abceshi-congling-daoyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.1"
