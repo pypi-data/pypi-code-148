33e0a7851646aea968cb657013f150543c1993dd
Tom Forbes <tom@tomforb.es>
2023-01-26 17:49:58 +0000

    {"name":"jishu-lingdaoli-300jiang","version":"2023.1.27.0","file":"jishu-lingdaoli-300jiang-2023.1.27.0.tar.gz","path":"jishu-lingdaoli-300jiang/jishu-lingdaoli-300jiang-2023.1.27.0.tar.gz"}

---
+++ jishu-lingdaoli-300jiang/jishu-lingdaoli-300jiang-2023.1.27.0.tar.gz/JishuLingdaoli300jiang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jishu-lingdaoli-300jiang
+https://github.com/apachecn/jishu-lingdaoli-300jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
