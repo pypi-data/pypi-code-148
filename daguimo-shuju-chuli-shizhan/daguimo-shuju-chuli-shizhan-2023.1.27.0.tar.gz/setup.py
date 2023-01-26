49eb2e4f2446ed094dfb820eefd7e7a9c3ad19bf
Tom Forbes <tom@tomforb.es>
2023-01-26 17:37:03 +0000

    {"name":"daguimo-shuju-chuli-shizhan","version":"2023.1.27.0","file":"daguimo-shuju-chuli-shizhan-2023.1.27.0.tar.gz","path":"daguimo-shuju-chuli-shizhan/daguimo-shuju-chuli-shizhan-2023.1.27.0.tar.gz"}

---
+++ daguimo-shuju-chuli-shizhan/daguimo-shuju-chuli-shizhan-2023.1.27.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DaguimoShujuChuliShizhan
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DaguimoShujuChuliShizhan'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="daguimo-shuju-chuli-shizhan",
+    version=DaguimoShujuChuliShizhan.__version__,
+    url="https://github.com/apachecn/daguimo-shuju-chuli-shizhan",
+    author=DaguimoShujuChuliShizhan.__author__,
+    author_email=DaguimoShujuChuliShizhan.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="36-大规模数据处理实战",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "daguimo-shuju-chuli-shizhan=DaguimoShujuChuliShizhan.__main__:main",
+            "DaguimoShujuChuliShizhan=DaguimoShujuChuliShizhan.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
