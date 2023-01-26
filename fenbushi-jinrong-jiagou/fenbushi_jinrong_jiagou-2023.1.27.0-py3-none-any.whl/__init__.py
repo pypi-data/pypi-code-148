c2751ea4cc6aa9983695d69a765781e243def0b2
Tom Forbes <tom@tomforb.es>
2023-01-26 17:41:24 +0000

    {"name":"fenbushi-jinrong-jiagou","version":"2023.1.27.0","file":"fenbushi_jinrong_jiagou-2023.1.27.0-py3-none-any.whl","path":"fenbushi-jinrong-jiagou/fenbushi_jinrong_jiagou-2023.1.27.0-py3-none-any.whl"}

---
+++ fenbushi-jinrong-jiagou/fenbushi_jinrong_jiagou-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fenbushi-jinrong-jiagou
+https://github.com/apachecn/fenbushi-jinrong-jiagou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
