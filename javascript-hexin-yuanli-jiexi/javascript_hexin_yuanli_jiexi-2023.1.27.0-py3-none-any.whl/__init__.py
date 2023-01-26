59b8ce4418e755268024eb4e441b981da434ebbf
Tom Forbes <tom@tomforb.es>
2023-01-26 17:47:28 +0000

    {"name":"javascript-hexin-yuanli-jiexi","version":"2023.1.27.0","file":"javascript_hexin_yuanli_jiexi-2023.1.27.0-py3-none-any.whl","path":"javascript-hexin-yuanli-jiexi/javascript_hexin_yuanli_jiexi-2023.1.27.0-py3-none-any.whl"}

---
+++ javascript-hexin-yuanli-jiexi/javascript_hexin_yuanli_jiexi-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""javascript-hexin-yuanli-jiexi
+https://github.com/apachecn/javascript-hexin-yuanli-jiexi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
