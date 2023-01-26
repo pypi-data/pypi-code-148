81973f7969bbbad79dbbf6913c00e85a0ac5116e
Tom Forbes <tom@tomforb.es>
2023-01-26 17:59:47 +0000

    {"name":"ruanjian-ceshi-52jiang","version":"2023.1.27.0","file":"ruanjian-ceshi-52jiang-2023.1.27.0.tar.gz","path":"ruanjian-ceshi-52jiang/ruanjian-ceshi-52jiang-2023.1.27.0.tar.gz"}

---
+++ ruanjian-ceshi-52jiang/ruanjian-ceshi-52jiang-2023.1.27.0.tar.gz/RuanjianCeshi52jiang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruanjian-ceshi-52jiang
+https://github.com/apachecn/ruanjian-ceshi-52jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
