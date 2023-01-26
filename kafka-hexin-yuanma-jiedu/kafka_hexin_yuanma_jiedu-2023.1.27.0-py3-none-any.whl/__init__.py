0d5691c937a513a5a462c4f95c68a67784aac340
Tom Forbes <tom@tomforb.es>
2023-01-26 17:52:05 +0000

    {"name":"kafka-hexin-yuanma-jiedu","version":"2023.1.27.0","file":"kafka_hexin_yuanma_jiedu-2023.1.27.0-py3-none-any.whl","path":"kafka-hexin-yuanma-jiedu/kafka_hexin_yuanma_jiedu-2023.1.27.0-py3-none-any.whl"}

---
+++ kafka-hexin-yuanma-jiedu/kafka_hexin_yuanma_jiedu-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kafka-hexin-yuanma-jiedu
+https://github.com/apachecn/kafka-hexin-yuanma-jiedu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
