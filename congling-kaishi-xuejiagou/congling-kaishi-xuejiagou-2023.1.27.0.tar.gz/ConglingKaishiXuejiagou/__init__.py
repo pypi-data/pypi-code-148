6e195186c582f74dbdda976650b2391bf1ef0017
Tom Forbes <tom@tomforb.es>
2023-01-26 17:35:18 +0000

    {"name":"congling-kaishi-xuejiagou","version":"2023.1.27.0","file":"congling-kaishi-xuejiagou-2023.1.27.0.tar.gz","path":"congling-kaishi-xuejiagou/congling-kaishi-xuejiagou-2023.1.27.0.tar.gz"}

---
+++ congling-kaishi-xuejiagou/congling-kaishi-xuejiagou-2023.1.27.0.tar.gz/ConglingKaishiXuejiagou/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""congling-kaishi-xuejiagou
+https://github.com/apachecn/congling-kaishi-xuejiagou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
