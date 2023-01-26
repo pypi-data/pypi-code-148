403187bf582cdaf971877e098992cc247851018c
Tom Forbes <tom@tomforb.es>
2023-01-26 17:54:03 +0000

    {"name":"mysql-bizhi-bihui","version":"2023.1.27.0","file":"mysql-bizhi-bihui-2023.1.27.0.tar.gz","path":"mysql-bizhi-bihui/mysql-bizhi-bihui-2023.1.27.0.tar.gz"}

---
+++ mysql-bizhi-bihui/mysql-bizhi-bihui-2023.1.27.0.tar.gz/MysqlBizhiBihui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mysql-bizhi-bihui
+https://github.com/apachecn/mysql-bizhi-bihui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
