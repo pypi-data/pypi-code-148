66dcbbd3b4e034cf459c0f2b666f17930df93907
Tom Forbes <tom@tomforb.es>
2023-01-26 18:22:35 +0000

    {"name":"zhaochengde-yunwei-tixi-guanlike","version":"2023.1.27.0","file":"zhaochengde_yunwei_tixi_guanlike-2023.1.27.0-py3-none-any.whl","path":"zhaochengde-yunwei-tixi-guanlike/zhaochengde_yunwei_tixi_guanlike-2023.1.27.0-py3-none-any.whl"}

---
+++ zhaochengde-yunwei-tixi-guanlike/zhaochengde_yunwei_tixi_guanlike-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhaochengde-yunwei-tixi-guanlike
+https://github.com/apachecn/zhaochengde-yunwei-tixi-guanlike"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
