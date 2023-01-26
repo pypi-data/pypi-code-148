96de9b7445be821ed613019ec7c0300d66546da0
Tom Forbes <tom@tomforb.es>
2023-01-26 22:02:46 +0000

    {"name":"sjtu-econmgt-invest-zhuojianwei","version":"2023.1.27.0","file":"sjtu_econmgt_invest_zhuojianwei-2023.1.27.0-py3-none-any.whl","path":"sjtu-econmgt-invest-zhuojianwei/sjtu_econmgt_invest_zhuojianwei-2023.1.27.0-py3-none-any.whl"}

---
+++ sjtu-econmgt-invest-zhuojianwei/sjtu_econmgt_invest_zhuojianwei-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-econmgt-invest-zhuojianwei
+https://github.com/apachecn/sjtu-econmgt-invest-zhuojianwei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
