fa41a83e8bba796325eceba70f69862426f8f7b5
Tom Forbes <tom@tomforb.es>
2023-01-26 17:36:36 +0000

    {"name":"dachang-jinsheng-zhinan","version":"2023.1.27.0","file":"dachang-jinsheng-zhinan-2023.1.27.0.tar.gz","path":"dachang-jinsheng-zhinan/dachang-jinsheng-zhinan-2023.1.27.0.tar.gz"}

---
+++ dachang-jinsheng-zhinan/dachang-jinsheng-zhinan-2023.1.27.0.tar.gz/DachangJinshengZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dachang-jinsheng-zhinan
+https://github.com/apachecn/dachang-jinsheng-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
