440ede4fa2870314a068c30cf9f27eedde0bd723
Tom Forbes <tom@tomforb.es>
2023-01-26 17:40:39 +0000

    {"name":"ddd-shizhanke","version":"2023.1.27.0","file":"ddd-shizhanke-2023.1.27.0.tar.gz","path":"ddd-shizhanke/ddd-shizhanke-2023.1.27.0.tar.gz"}

---
+++ ddd-shizhanke/ddd-shizhanke-2023.1.27.0.tar.gz/DddShizhanke/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ddd-shizhanke
+https://github.com/apachecn/ddd-shizhanke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
