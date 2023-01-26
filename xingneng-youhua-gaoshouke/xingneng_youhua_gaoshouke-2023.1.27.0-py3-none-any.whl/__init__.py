ad4ec7c98cb29b0392a0896e02b8bfb6b492f55e
Tom Forbes <tom@tomforb.es>
2023-01-26 18:21:13 +0000

    {"name":"xingneng-youhua-gaoshouke","version":"2023.1.27.0","file":"xingneng_youhua_gaoshouke-2023.1.27.0-py3-none-any.whl","path":"xingneng-youhua-gaoshouke/xingneng_youhua_gaoshouke-2023.1.27.0-py3-none-any.whl"}

---
+++ xingneng-youhua-gaoshouke/xingneng_youhua_gaoshouke-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xingneng-youhua-gaoshouke
+https://github.com/apachecn/xingneng-youhua-gaoshouke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
