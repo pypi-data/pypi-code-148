558173e8120e05ff15a0026c4c04b8c9a430f961
Tom Forbes <tom@tomforb.es>
2023-01-26 17:33:25 +0000

    {"name":"chengwei-ai-chanpinjingli","version":"2023.1.27.0","file":"chengwei_ai_chanpinjingli-2023.1.27.0-py3-none-any.whl","path":"chengwei-ai-chanpinjingli/chengwei_ai_chanpinjingli-2023.1.27.0-py3-none-any.whl"}

---
+++ chengwei-ai-chanpinjingli/chengwei_ai_chanpinjingli-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chengwei-ai-chanpinjingli
+https://github.com/apachecn/chengwei-ai-chanpinjingli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
