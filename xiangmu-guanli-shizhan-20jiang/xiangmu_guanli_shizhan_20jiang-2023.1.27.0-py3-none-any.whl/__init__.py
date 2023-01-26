c257f8debad062ce3eee0a01140b1d2bd09ba712
Tom Forbes <tom@tomforb.es>
2023-01-26 18:20:21 +0000

    {"name":"xiangmu-guanli-shizhan-20jiang","version":"2023.1.27.0","file":"xiangmu_guanli_shizhan_20jiang-2023.1.27.0-py3-none-any.whl","path":"xiangmu-guanli-shizhan-20jiang/xiangmu_guanli_shizhan_20jiang-2023.1.27.0-py3-none-any.whl"}

---
+++ xiangmu-guanli-shizhan-20jiang/xiangmu_guanli_shizhan_20jiang-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiangmu-guanli-shizhan-20jiang
+https://github.com/apachecn/xiangmu-guanli-shizhan-20jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
