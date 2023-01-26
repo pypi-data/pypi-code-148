dc0556f1645bd41cd2ed8f3d559309a29674f3dc
Tom Forbes <tom@tomforb.es>
2023-01-26 18:06:14 +0000

    {"name":"shenru-chaijie-java-xuniji","version":"2023.1.27.0","file":"shenru_chaijie_java_xuniji-2023.1.27.0-py3-none-any.whl","path":"shenru-chaijie-java-xuniji/shenru_chaijie_java_xuniji-2023.1.27.0-py3-none-any.whl"}

---
+++ shenru-chaijie-java-xuniji/shenru_chaijie_java_xuniji-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenru-chaijie-java-xuniji
+https://github.com/apachecn/shenru-chaijie-java-xuniji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
