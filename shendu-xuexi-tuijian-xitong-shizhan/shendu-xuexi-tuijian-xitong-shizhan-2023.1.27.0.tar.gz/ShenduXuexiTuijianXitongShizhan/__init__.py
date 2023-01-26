7c855798b81559fc8b544e2e561cdcd402e9ef24
Tom Forbes <tom@tomforb.es>
2023-01-26 18:05:53 +0000

    {"name":"shendu-xuexi-tuijian-xitong-shizhan","version":"2023.1.27.0","file":"shendu-xuexi-tuijian-xitong-shizhan-2023.1.27.0.tar.gz","path":"shendu-xuexi-tuijian-xitong-shizhan/shendu-xuexi-tuijian-xitong-shizhan-2023.1.27.0.tar.gz"}

---
+++ shendu-xuexi-tuijian-xitong-shizhan/shendu-xuexi-tuijian-xitong-shizhan-2023.1.27.0.tar.gz/ShenduXuexiTuijianXitongShizhan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shendu-xuexi-tuijian-xitong-shizhan
+https://github.com/apachecn/shendu-xuexi-tuijian-xitong-shizhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
