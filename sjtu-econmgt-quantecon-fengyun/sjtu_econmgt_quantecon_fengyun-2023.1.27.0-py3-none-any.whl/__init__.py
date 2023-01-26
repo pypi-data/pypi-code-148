513d7401dd79fb11df0ede369c1ffa703eea5eba
Tom Forbes <tom@tomforb.es>
2023-01-26 22:10:03 +0000

    {"name":"sjtu-econmgt-quantecon-fengyun","version":"2023.1.27.0","file":"sjtu_econmgt_quantecon_fengyun-2023.1.27.0-py3-none-any.whl","path":"sjtu-econmgt-quantecon-fengyun/sjtu_econmgt_quantecon_fengyun-2023.1.27.0-py3-none-any.whl"}

---
+++ sjtu-econmgt-quantecon-fengyun/sjtu_econmgt_quantecon_fengyun-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-econmgt-quantecon-fengyun
+https://github.com/apachecn/sjtu-econmgt-quantecon-fengyun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
