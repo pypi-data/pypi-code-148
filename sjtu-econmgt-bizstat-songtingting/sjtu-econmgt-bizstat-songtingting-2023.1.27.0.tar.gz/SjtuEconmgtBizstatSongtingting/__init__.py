61408a230e738ae062024783122d766ea8cabfc5
Tom Forbes <tom@tomforb.es>
2023-01-26 21:51:01 +0000

    {"name":"sjtu-econmgt-bizstat-songtingting","version":"2023.1.27.0","file":"sjtu-econmgt-bizstat-songtingting-2023.1.27.0.tar.gz","path":"sjtu-econmgt-bizstat-songtingting/sjtu-econmgt-bizstat-songtingting-2023.1.27.0.tar.gz"}

---
+++ sjtu-econmgt-bizstat-songtingting/sjtu-econmgt-bizstat-songtingting-2023.1.27.0.tar.gz/SjtuEconmgtBizstatSongtingting/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-econmgt-bizstat-songtingting
+https://github.com/apachecn/sjtu-econmgt-bizstat-songtingting"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
