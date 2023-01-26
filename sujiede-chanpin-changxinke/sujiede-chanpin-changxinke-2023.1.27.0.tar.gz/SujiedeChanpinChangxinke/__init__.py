5da96bbf5a08586cdd04c9a9486138e58fcf42ac
Tom Forbes <tom@tomforb.es>
2023-01-26 18:17:46 +0000

    {"name":"sujiede-chanpin-changxinke","version":"2023.1.27.0","file":"sujiede-chanpin-changxinke-2023.1.27.0.tar.gz","path":"sujiede-chanpin-changxinke/sujiede-chanpin-changxinke-2023.1.27.0.tar.gz"}

---
+++ sujiede-chanpin-changxinke/sujiede-chanpin-changxinke-2023.1.27.0.tar.gz/SujiedeChanpinChangxinke/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sujiede-chanpin-changxinke
+https://github.com/apachecn/sujiede-chanpin-changxinke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
