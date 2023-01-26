6eb94cf0198938be9ba191fdb747defe188c6744
Tom Forbes <tom@tomforb.es>
2023-01-26 18:17:06 +0000

    {"name":"sre-shizhan-shouce","version":"2023.1.27.0","file":"sre_shizhan_shouce-2023.1.27.0-py3-none-any.whl","path":"sre-shizhan-shouce/sre_shizhan_shouce-2023.1.27.0-py3-none-any.whl"}

---
+++ sre-shizhan-shouce/sre_shizhan_shouce-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sre-shizhan-shouce
+https://github.com/apachecn/sre-shizhan-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
