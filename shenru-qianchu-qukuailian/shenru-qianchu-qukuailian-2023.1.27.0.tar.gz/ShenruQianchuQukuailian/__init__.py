61e9bbae590698cefb9340291d2cd3a746bcff9c
Tom Forbes <tom@tomforb.es>
2023-01-26 18:07:06 +0000

    {"name":"shenru-qianchu-qukuailian","version":"2023.1.27.0","file":"shenru-qianchu-qukuailian-2023.1.27.0.tar.gz","path":"shenru-qianchu-qukuailian/shenru-qianchu-qukuailian-2023.1.27.0.tar.gz"}

---
+++ shenru-qianchu-qukuailian/shenru-qianchu-qukuailian-2023.1.27.0.tar.gz/ShenruQianchuQukuailian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenru-qianchu-qukuailian
+https://github.com/apachecn/shenru-qianchu-qukuailian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
