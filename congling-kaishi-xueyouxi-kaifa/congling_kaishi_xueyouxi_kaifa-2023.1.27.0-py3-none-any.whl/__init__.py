f215e4cc237f13580f3554fa735414040c70ca91
Tom Forbes <tom@tomforb.es>
2023-01-26 17:35:39 +0000

    {"name":"congling-kaishi-xueyouxi-kaifa","version":"2023.1.27.0","file":"congling_kaishi_xueyouxi_kaifa-2023.1.27.0-py3-none-any.whl","path":"congling-kaishi-xueyouxi-kaifa/congling_kaishi_xueyouxi_kaifa-2023.1.27.0-py3-none-any.whl"}

---
+++ congling-kaishi-xueyouxi-kaifa/congling_kaishi_xueyouxi_kaifa-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""congling-kaishi-xueyouxi-kaifa
+https://github.com/apachecn/congling-kaishi-xueyouxi-kaifa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
