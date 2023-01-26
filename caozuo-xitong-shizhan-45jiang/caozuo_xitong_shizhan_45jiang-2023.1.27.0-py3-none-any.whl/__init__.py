5e37b47ecfa94c0a9cf365e1e81fa7e9161e8277
Tom Forbes <tom@tomforb.es>
2023-01-26 17:33:05 +0000

    {"name":"caozuo-xitong-shizhan-45jiang","version":"2023.1.27.0","file":"caozuo_xitong_shizhan_45jiang-2023.1.27.0-py3-none-any.whl","path":"caozuo-xitong-shizhan-45jiang/caozuo_xitong_shizhan_45jiang-2023.1.27.0-py3-none-any.whl"}

---
+++ caozuo-xitong-shizhan-45jiang/caozuo_xitong_shizhan_45jiang-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""caozuo-xitong-shizhan-45jiang
+https://github.com/apachecn/caozuo-xitong-shizhan-45jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
