6856f62c44975c714fe133b8c56739aeb86bed2c
Tom Forbes <tom@tomforb.es>
2023-01-26 17:55:28 +0000

    {"name":"qiaoxinliangde-cto-chengzhang-fupan","version":"2023.1.27.0","file":"qiaoxinliangde-cto-chengzhang-fupan-2023.1.27.0.tar.gz","path":"qiaoxinliangde-cto-chengzhang-fupan/qiaoxinliangde-cto-chengzhang-fupan-2023.1.27.0.tar.gz"}

---
+++ qiaoxinliangde-cto-chengzhang-fupan/qiaoxinliangde-cto-chengzhang-fupan-2023.1.27.0.tar.gz/QiaoxinliangdeCtoChengzhangFupan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qiaoxinliangde-cto-chengzhang-fupan
+https://github.com/apachecn/qiaoxinliangde-cto-chengzhang-fupan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
