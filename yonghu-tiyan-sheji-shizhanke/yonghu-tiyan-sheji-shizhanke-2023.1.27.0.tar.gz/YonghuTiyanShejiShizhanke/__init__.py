27d00344b8d6aee09e895b2aaf1c2194b488ce1e
Tom Forbes <tom@tomforb.es>
2023-01-26 18:22:26 +0000

    {"name":"yonghu-tiyan-sheji-shizhanke","version":"2023.1.27.0","file":"yonghu-tiyan-sheji-shizhanke-2023.1.27.0.tar.gz","path":"yonghu-tiyan-sheji-shizhanke/yonghu-tiyan-sheji-shizhanke-2023.1.27.0.tar.gz"}

---
+++ yonghu-tiyan-sheji-shizhanke/yonghu-tiyan-sheji-shizhanke-2023.1.27.0.tar.gz/YonghuTiyanShejiShizhanke/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yonghu-tiyan-sheji-shizhanke
+https://github.com/apachecn/yonghu-tiyan-sheji-shizhanke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
