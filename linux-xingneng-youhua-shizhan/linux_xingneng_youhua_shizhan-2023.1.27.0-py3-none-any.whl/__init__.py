225490ad2cd46493b2fedbbc329b5736bff1e145
Tom Forbes <tom@tomforb.es>
2023-01-26 17:52:49 +0000

    {"name":"linux-xingneng-youhua-shizhan","version":"2023.1.27.0","file":"linux_xingneng_youhua_shizhan-2023.1.27.0-py3-none-any.whl","path":"linux-xingneng-youhua-shizhan/linux_xingneng_youhua_shizhan-2023.1.27.0-py3-none-any.whl"}

---
+++ linux-xingneng-youhua-shizhan/linux_xingneng_youhua_shizhan-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""linux-xingneng-youhua-shizhan
+https://github.com/apachecn/linux-xingneng-youhua-shizhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
