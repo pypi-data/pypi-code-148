02b2c00a4470c06a17f813b643124de4ab126d8d
Tom Forbes <tom@tomforb.es>
2023-01-26 18:22:49 +0000

    {"name":"zhengze-biaodashi-rumenke","version":"2023.1.27.0","file":"zhengze-biaodashi-rumenke-2023.1.27.0.tar.gz","path":"zhengze-biaodashi-rumenke/zhengze-biaodashi-rumenke-2023.1.27.0.tar.gz"}

---
+++ zhengze-biaodashi-rumenke/zhengze-biaodashi-rumenke-2023.1.27.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ZhengzeBiaodashiRumenke
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ZhengzeBiaodashiRumenke'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="zhengze-biaodashi-rumenke",
+    version=ZhengzeBiaodashiRumenke.__version__,
+    url="https://github.com/apachecn/zhengze-biaodashi-rumenke",
+    author=ZhengzeBiaodashiRumenke.__author__,
+    author_email=ZhengzeBiaodashiRumenke.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="137-正则表达式入门课",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "zhengze-biaodashi-rumenke=ZhengzeBiaodashiRumenke.__main__:main",
+            "ZhengzeBiaodashiRumenke=ZhengzeBiaodashiRumenke.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
