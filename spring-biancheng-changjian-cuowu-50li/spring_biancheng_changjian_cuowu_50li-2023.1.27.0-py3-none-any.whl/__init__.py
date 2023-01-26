5b47cfac4593a807280f8fb640d5788510222840
Tom Forbes <tom@tomforb.es>
2023-01-26 18:16:06 +0000

    {"name":"spring-biancheng-changjian-cuowu-50li","version":"2023.1.27.0","file":"spring_biancheng_changjian_cuowu_50li-2023.1.27.0-py3-none-any.whl","path":"spring-biancheng-changjian-cuowu-50li/spring_biancheng_changjian_cuowu_50li-2023.1.27.0-py3-none-any.whl"}

---
+++ spring-biancheng-changjian-cuowu-50li/spring_biancheng_changjian_cuowu_50li-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""spring-biancheng-changjian-cuowu-50li
+https://github.com/apachecn/spring-biancheng-changjian-cuowu-50li"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
