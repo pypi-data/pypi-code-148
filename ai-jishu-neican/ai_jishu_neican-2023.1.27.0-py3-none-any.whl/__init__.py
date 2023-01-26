19c4a426472118b8e32ba4802bbf043914ddbd6d
Tom Forbes <tom@tomforb.es>
2023-01-26 17:30:15 +0000

    {"name":"ai-jishu-neican","version":"2023.1.27.0","file":"ai_jishu_neican-2023.1.27.0-py3-none-any.whl","path":"ai-jishu-neican/ai_jishu_neican-2023.1.27.0-py3-none-any.whl"}

---
+++ ai-jishu-neican/ai_jishu_neican-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ai-jishu-neican
+https://github.com/apachecn/ai-jishu-neican"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
