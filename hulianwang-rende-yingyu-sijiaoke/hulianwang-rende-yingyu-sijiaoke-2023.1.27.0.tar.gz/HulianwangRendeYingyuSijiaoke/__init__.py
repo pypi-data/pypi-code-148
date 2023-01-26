156929e66204e721e0b54c62997aaff81be61801
Tom Forbes <tom@tomforb.es>
2023-01-26 17:45:33 +0000

    {"name":"hulianwang-rende-yingyu-sijiaoke","version":"2023.1.27.0","file":"hulianwang-rende-yingyu-sijiaoke-2023.1.27.0.tar.gz","path":"hulianwang-rende-yingyu-sijiaoke/hulianwang-rende-yingyu-sijiaoke-2023.1.27.0.tar.gz"}

---
+++ hulianwang-rende-yingyu-sijiaoke/hulianwang-rende-yingyu-sijiaoke-2023.1.27.0.tar.gz/HulianwangRendeYingyuSijiaoke/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hulianwang-rende-yingyu-sijiaoke
+https://github.com/apachecn/hulianwang-rende-yingyu-sijiaoke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
