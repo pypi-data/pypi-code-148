304d143c25a0e74bba47df6f132044497315c07f
Tom Forbes <tom@tomforb.es>
2023-01-26 17:54:30 +0000

    {"name":"mysql-shizhan-45jiang","version":"2023.1.27.0","file":"mysql-shizhan-45jiang-2023.1.27.0.tar.gz","path":"mysql-shizhan-45jiang/mysql-shizhan-45jiang-2023.1.27.0.tar.gz"}

---
+++ mysql-shizhan-45jiang/mysql-shizhan-45jiang-2023.1.27.0.tar.gz/MysqlShizhan45jiang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mysql-shizhan-45jiang
+https://github.com/apachecn/mysql-shizhan-45jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
