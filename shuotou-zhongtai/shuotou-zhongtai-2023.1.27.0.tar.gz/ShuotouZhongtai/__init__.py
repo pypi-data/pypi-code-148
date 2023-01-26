73d32fb808cf5661bc73a991acb10b330bbc3f3a
Tom Forbes <tom@tomforb.es>
2023-01-26 18:15:41 +0000

    {"name":"shuotou-zhongtai","version":"2023.1.27.0","file":"shuotou-zhongtai-2023.1.27.0.tar.gz","path":"shuotou-zhongtai/shuotou-zhongtai-2023.1.27.0.tar.gz"}

---
+++ shuotou-zhongtai/shuotou-zhongtai-2023.1.27.0.tar.gz/ShuotouZhongtai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuotou-zhongtai
+https://github.com/apachecn/shuotou-zhongtai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
