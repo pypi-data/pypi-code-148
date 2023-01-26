94d724aa9b1a835a9c49a06aedaf40f69692ed8e
Tom Forbes <tom@tomforb.es>
2023-01-26 17:50:56 +0000

    {"name":"jiyu-renyinde-yonghu-tiyan-shejike","version":"2023.1.27.0","file":"jiyu_renyinde_yonghu_tiyan_shejike-2023.1.27.0-py3-none-any.whl","path":"jiyu-renyinde-yonghu-tiyan-shejike/jiyu_renyinde_yonghu_tiyan_shejike-2023.1.27.0-py3-none-any.whl"}

---
+++ jiyu-renyinde-yonghu-tiyan-shejike/jiyu_renyinde_yonghu_tiyan_shejike-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiyu-renyinde-yonghu-tiyan-shejike
+https://github.com/apachecn/jiyu-renyinde-yonghu-tiyan-shejike"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
