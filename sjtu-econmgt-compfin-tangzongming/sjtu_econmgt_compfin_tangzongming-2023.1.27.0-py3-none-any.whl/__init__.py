562b1d3015036a174164c337206eeae2a3d37f61
Tom Forbes <tom@tomforb.es>
2023-01-26 21:52:35 +0000

    {"name":"sjtu-econmgt-compfin-tangzongming","version":"2023.1.27.0","file":"sjtu_econmgt_compfin_tangzongming-2023.1.27.0-py3-none-any.whl","path":"sjtu-econmgt-compfin-tangzongming/sjtu_econmgt_compfin_tangzongming-2023.1.27.0-py3-none-any.whl"}

---
+++ sjtu-econmgt-compfin-tangzongming/sjtu_econmgt_compfin_tangzongming-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-econmgt-compfin-tangzongming
+https://github.com/apachecn/sjtu-econmgt-compfin-tangzongming"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
