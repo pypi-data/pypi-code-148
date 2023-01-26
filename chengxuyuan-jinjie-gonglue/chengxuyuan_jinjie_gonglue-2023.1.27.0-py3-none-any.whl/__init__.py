d15b6cc20c2553feeeefc83b065b8ae4a2f18c97
Tom Forbes <tom@tomforb.es>
2023-01-26 17:33:40 +0000

    {"name":"chengxuyuan-jinjie-gonglue","version":"2023.1.27.0","file":"chengxuyuan_jinjie_gonglue-2023.1.27.0-py3-none-any.whl","path":"chengxuyuan-jinjie-gonglue/chengxuyuan_jinjie_gonglue-2023.1.27.0-py3-none-any.whl"}

---
+++ chengxuyuan-jinjie-gonglue/chengxuyuan_jinjie_gonglue-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chengxuyuan-jinjie-gonglue
+https://github.com/apachecn/chengxuyuan-jinjie-gonglue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
