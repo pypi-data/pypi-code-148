f2ae9574f2a8e75b5ba2a28214b955b5c73d80cd
Tom Forbes <tom@tomforb.es>
2023-01-26 17:31:36 +0000

    {"name":"anquan-gongfang-jineng-36jiang","version":"2023.1.27.0","file":"anquan_gongfang_jineng_36jiang-2023.1.27.0-py3-none-any.whl","path":"anquan-gongfang-jineng-36jiang/anquan_gongfang_jineng_36jiang-2023.1.27.0-py3-none-any.whl"}

---
+++ anquan-gongfang-jineng-36jiang/anquan_gongfang_jineng_36jiang-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""anquan-gongfang-jineng-36jiang
+https://github.com/apachecn/anquan-gongfang-jineng-36jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
