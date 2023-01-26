5fc0833293b4ab74ea9cd533067b7defc053d0bf
Tom Forbes <tom@tomforb.es>
2023-01-26 17:47:49 +0000

    {"name":"jianghao-gushi","version":"2023.1.27.0","file":"jianghao_gushi-2023.1.27.0-py3-none-any.whl","path":"jianghao-gushi/jianghao_gushi-2023.1.27.0-py3-none-any.whl"}

---
+++ jianghao-gushi/jianghao_gushi-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jianghao-gushi
+https://github.com/apachecn/jianghao-gushi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
