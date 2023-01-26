e52cb425ac3bd109ff56a910f5917c2001682fe6
Tom Forbes <tom@tomforb.es>
2023-01-26 17:34:16 +0000

    {"name":"chixu-jiaofu-36jiang","version":"2023.1.27.0","file":"chixu-jiaofu-36jiang-2023.1.27.0.tar.gz","path":"chixu-jiaofu-36jiang/chixu-jiaofu-36jiang-2023.1.27.0.tar.gz"}

---
+++ chixu-jiaofu-36jiang/chixu-jiaofu-36jiang-2023.1.27.0.tar.gz/ChixuJiaofu36jiang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chixu-jiaofu-36jiang
+https://github.com/apachecn/chixu-jiaofu-36jiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
