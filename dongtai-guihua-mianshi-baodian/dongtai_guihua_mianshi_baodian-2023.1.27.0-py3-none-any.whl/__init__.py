98897afd88ce53e6350ddb1c5c683d810e2b86cd
Tom Forbes <tom@tomforb.es>
2023-01-26 17:40:59 +0000

    {"name":"dongtai-guihua-mianshi-baodian","version":"2023.1.27.0","file":"dongtai_guihua_mianshi_baodian-2023.1.27.0-py3-none-any.whl","path":"dongtai-guihua-mianshi-baodian/dongtai_guihua_mianshi_baodian-2023.1.27.0-py3-none-any.whl"}

---
+++ dongtai-guihua-mianshi-baodian/dongtai_guihua_mianshi_baodian-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dongtai-guihua-mianshi-baodian
+https://github.com/apachecn/dongtai-guihua-mianshi-baodian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
