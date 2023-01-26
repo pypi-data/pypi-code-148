d6e6f36bfd2b81b3999e0fc4fa0ed1364ed21635
Tom Forbes <tom@tomforb.es>
2023-01-26 17:40:48 +0000

    {"name":"devops-shizhan-biji","version":"2023.1.27.0","file":"devops_shizhan_biji-2023.1.27.0-py3-none-any.whl","path":"devops-shizhan-biji/devops_shizhan_biji-2023.1.27.0-py3-none-any.whl"}

---
+++ devops-shizhan-biji/devops_shizhan_biji-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""devops-shizhan-biji
+https://github.com/apachecn/devops-shizhan-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
