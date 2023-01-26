84034a618718765c74ef7e9df0a5cde29b8be5ac
Tom Forbes <tom@tomforb.es>
2023-01-26 21:58:58 +0000

    {"name":"sjtu-econmgt-econ-xuli","version":"2023.1.27.0","file":"sjtu_econmgt_econ_xuli-2023.1.27.0-py3-none-any.whl","path":"sjtu-econmgt-econ-xuli/sjtu_econmgt_econ_xuli-2023.1.27.0-py3-none-any.whl"}

---
+++ sjtu-econmgt-econ-xuli/sjtu_econmgt_econ_xuli-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-econmgt-econ-xuli
+https://github.com/apachecn/sjtu-econmgt-econ-xuli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
