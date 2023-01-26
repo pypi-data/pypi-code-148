82e5ddac01c3222161c643a94c9874602f7bfa73
Tom Forbes <tom@tomforb.es>
2023-01-26 17:54:54 +0000

    {"name":"openresty-congrumen-daoshizhan","version":"2023.1.27.0","file":"openresty-congrumen-daoshizhan-2023.1.27.0.tar.gz","path":"openresty-congrumen-daoshizhan/openresty-congrumen-daoshizhan-2023.1.27.0.tar.gz"}

---
+++ openresty-congrumen-daoshizhan/openresty-congrumen-daoshizhan-2023.1.27.0.tar.gz/OpenrestyCongrumenDaoshizhan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""openresty-congrumen-daoshizhan
+https://github.com/apachecn/openresty-congrumen-daoshizhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
