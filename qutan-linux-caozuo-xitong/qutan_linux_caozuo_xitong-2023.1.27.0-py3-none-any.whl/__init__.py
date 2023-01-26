26d3fcd97fe6134f919d589bf5c6504b7273fa39
Tom Forbes <tom@tomforb.es>
2023-01-26 17:56:52 +0000

    {"name":"qutan-linux-caozuo-xitong","version":"2023.1.27.0","file":"qutan_linux_caozuo_xitong-2023.1.27.0-py3-none-any.whl","path":"qutan-linux-caozuo-xitong/qutan_linux_caozuo_xitong-2023.1.27.0-py3-none-any.whl"}

---
+++ qutan-linux-caozuo-xitong/qutan_linux_caozuo_xitong-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qutan-linux-caozuo-xitong
+https://github.com/apachecn/qutan-linux-caozuo-xitong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
