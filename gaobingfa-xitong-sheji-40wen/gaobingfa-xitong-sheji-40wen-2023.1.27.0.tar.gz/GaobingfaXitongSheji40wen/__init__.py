7094394a7c042669becac73f2d9b99d4e26e79e1
Tom Forbes <tom@tomforb.es>
2023-01-26 17:42:32 +0000

    {"name":"gaobingfa-xitong-sheji-40wen","version":"2023.1.27.0","file":"gaobingfa-xitong-sheji-40wen-2023.1.27.0.tar.gz","path":"gaobingfa-xitong-sheji-40wen/gaobingfa-xitong-sheji-40wen-2023.1.27.0.tar.gz"}

---
+++ gaobingfa-xitong-sheji-40wen/gaobingfa-xitong-sheji-40wen-2023.1.27.0.tar.gz/GaobingfaXitongSheji40wen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gaobingfa-xitong-sheji-40wen
+https://github.com/apachecn/gaobingfa-xitong-sheji-40wen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
