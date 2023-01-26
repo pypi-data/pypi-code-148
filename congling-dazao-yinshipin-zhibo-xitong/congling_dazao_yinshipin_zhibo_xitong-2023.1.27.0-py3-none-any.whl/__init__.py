ef1c559ce3ba72547984cd4fd8f8e2d40ba216c1
Tom Forbes <tom@tomforb.es>
2023-01-26 17:34:48 +0000

    {"name":"congling-dazao-yinshipin-zhibo-xitong","version":"2023.1.27.0","file":"congling_dazao_yinshipin_zhibo_xitong-2023.1.27.0-py3-none-any.whl","path":"congling-dazao-yinshipin-zhibo-xitong/congling_dazao_yinshipin_zhibo_xitong-2023.1.27.0-py3-none-any.whl"}

---
+++ congling-dazao-yinshipin-zhibo-xitong/congling_dazao_yinshipin_zhibo_xitong-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""congling-dazao-yinshipin-zhibo-xitong
+https://github.com/apachecn/congling-dazao-yinshipin-zhibo-xitong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
