0ceb2317014582c9d7f0774025d0e80693ee775d
Tom Forbes <tom@tomforb.es>
2023-01-26 17:42:09 +0000

    {"name":"fenbushi-xieyi-yusuanfa-shizhan","version":"2023.1.27.0","file":"fenbushi-xieyi-yusuanfa-shizhan-2023.1.27.0.tar.gz","path":"fenbushi-xieyi-yusuanfa-shizhan/fenbushi-xieyi-yusuanfa-shizhan-2023.1.27.0.tar.gz"}

---
+++ fenbushi-xieyi-yusuanfa-shizhan/fenbushi-xieyi-yusuanfa-shizhan-2023.1.27.0.tar.gz/FenbushiXieyiYusuanfaShizhan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fenbushi-xieyi-yusuanfa-shizhan
+https://github.com/apachecn/fenbushi-xieyi-yusuanfa-shizhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
