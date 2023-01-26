3bd8fd10bf617143a6063941be1b639b443d21a2
Tom Forbes <tom@tomforb.es>
2023-01-26 17:30:56 +0000

    {"name":"aishang-paobu","version":"2023.1.27.0","file":"aishang_paobu-2023.1.27.0-py3-none-any.whl","path":"aishang-paobu/aishang_paobu-2023.1.27.0-py3-none-any.whl"}

---
+++ aishang-paobu/aishang_paobu-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aishang-paobu
+https://github.com/apachecn/aishang-paobu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
