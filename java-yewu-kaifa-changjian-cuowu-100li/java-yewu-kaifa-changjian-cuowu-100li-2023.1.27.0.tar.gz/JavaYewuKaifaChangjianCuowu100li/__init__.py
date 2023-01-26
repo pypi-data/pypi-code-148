81abf5b31b193ee0bb728bd9ca2eae5c2981c7e0
Tom Forbes <tom@tomforb.es>
2023-01-26 17:47:20 +0000

    {"name":"java-yewu-kaifa-changjian-cuowu-100li","version":"2023.1.27.0","file":"java-yewu-kaifa-changjian-cuowu-100li-2023.1.27.0.tar.gz","path":"java-yewu-kaifa-changjian-cuowu-100li/java-yewu-kaifa-changjian-cuowu-100li-2023.1.27.0.tar.gz"}

---
+++ java-yewu-kaifa-changjian-cuowu-100li/java-yewu-kaifa-changjian-cuowu-100li-2023.1.27.0.tar.gz/JavaYewuKaifaChangjianCuowu100li/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-yewu-kaifa-changjian-cuowu-100li
+https://github.com/apachecn/java-yewu-kaifa-changjian-cuowu-100li"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
