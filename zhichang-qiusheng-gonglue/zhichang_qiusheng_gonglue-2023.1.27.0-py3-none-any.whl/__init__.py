801a1c1944d4faa46a23610d78d06e8dc07526c3
Tom Forbes <tom@tomforb.es>
2023-01-26 18:22:56 +0000

    {"name":"zhichang-qiusheng-gonglue","version":"2023.1.27.0","file":"zhichang_qiusheng_gonglue-2023.1.27.0-py3-none-any.whl","path":"zhichang-qiusheng-gonglue/zhichang_qiusheng_gonglue-2023.1.27.0-py3-none-any.whl"}

---
+++ zhichang-qiusheng-gonglue/zhichang_qiusheng_gonglue-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhichang-qiusheng-gonglue
+https://github.com/apachecn/zhichang-qiusheng-gonglue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
