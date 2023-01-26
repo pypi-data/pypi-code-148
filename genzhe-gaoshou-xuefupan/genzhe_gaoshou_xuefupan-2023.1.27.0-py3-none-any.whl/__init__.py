1f957b6d1009fed929d3869ee1ddbf79d7059e92
Tom Forbes <tom@tomforb.es>
2023-01-26 17:43:42 +0000

    {"name":"genzhe-gaoshou-xuefupan","version":"2023.1.27.0","file":"genzhe_gaoshou_xuefupan-2023.1.27.0-py3-none-any.whl","path":"genzhe-gaoshou-xuefupan/genzhe_gaoshou_xuefupan-2023.1.27.0-py3-none-any.whl"}

---
+++ genzhe-gaoshou-xuefupan/genzhe_gaoshou_xuefupan-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""genzhe-gaoshou-xuefupan
+https://github.com/apachecn/genzhe-gaoshou-xuefupan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
