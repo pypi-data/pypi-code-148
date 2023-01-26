b68327d6497e79a912bc2a504414ca26c6a5b306
Tom Forbes <tom@tomforb.es>
2023-01-26 18:06:50 +0000

    {"name":"shenru-pouxi-k8s","version":"2023.1.27.0","file":"shenru_pouxi_k8s-2023.1.27.0-py3-none-any.whl","path":"shenru-pouxi-k8s/shenru_pouxi_k8s-2023.1.27.0-py3-none-any.whl"}

---
+++ shenru-pouxi-k8s/shenru_pouxi_k8s-2023.1.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenru-pouxi-k8s
+https://github.com/apachecn/shenru-pouxi-k8s"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.27.0"
