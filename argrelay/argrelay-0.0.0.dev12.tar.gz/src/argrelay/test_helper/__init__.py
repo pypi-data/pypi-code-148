285f0997eb44eab778d4fa164583c3fa1c643e10
Tom Forbes <tom@tomforb.es>
2023-01-28 18:01:33 +0000

    {"name":"argrelay","version":"0.0.0.dev12","file":"argrelay-0.0.0.dev12.tar.gz","path":"argrelay/argrelay-0.0.0.dev12.tar.gz"}

---
+++ argrelay/argrelay-0.0.0.dev12.tar.gz/src/argrelay/test_helper/__init__.py
@@ -0,0 +1,52 @@
+"""
+"""
+import os
+from inspect import getframeinfo, currentframe
+
+test_data_ = "test_data"
+
+# Enable/disable breakpoints programmatically:
+__is_breakpoint_enabled = False
+
+
+def enable_breakpoint():
+    global __is_breakpoint_enabled
+    __is_breakpoint_enabled = True
+
+
+def disable_breakpoint():
+    global __is_breakpoint_enabled
+    __is_breakpoint_enabled = False
+
+
+def is_breakpoint_enabled() -> bool:
+    return __is_breakpoint_enabled
+
+
+def line_no() -> int:
+    """
+    Get source line in the caller frame
+    """
+    return getframeinfo(currentframe().f_back).lineno
+
+
+def parse_line_and_cpos(test_line: str) -> (str, int):
+    """
+    Translate test line with pipe as cursor place into command line string and cursor char position (cpos)
+    """
+    assert test_line.count('|') == 1
+    cursor_cpos = test_line.index('|')
+    command_line = test_line.replace('|', "")
+    return command_line, cursor_cpos
+
+
+def change_to_known_path_tests_dir():
+    """
+    This function changes to known path (`./test`) within repo root.
+
+    This allows any other code relying on the file access within the repo use reliable relative paths.
+    """
+
+    # When IDE runs, CWD = "tests", when `tox` runs, CWD = [repo root], change to `tests` subdir:
+    if os.path.basename(os.getcwd()) != "tests":
+        os.chdir("tests")
